ad0deef750de6b8fca88d5a4b1e82a36a6804eb6
Kowyzu <kowyzu@gmail.com>
2025-06-30 22:15:17 +0200

    initiate new project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <section class="section section-hero"></section>
+  </header>
+
+  <main>
+    <section class="section section-features"></section>
+    <section class="section section-call-to-action"></section>
+    <section class="section section-contact"></section>
+    <section class="section section-pricing"></section>
+
+  </main>
+
+  <footer>
+    <!-- footer placeholder -->
+  </footer>
+</template>
+
+

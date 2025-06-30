3100b06a74dc2f47e9dc7f4daadd26cd74ec80ba
Kowyzu <kowyzu@gmail.com>
2025-06-30 23:49:24 +0200

    feat: draft structure of hero section

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,32 @@
 
 <template>
   <header>
-    <section class="section section-hero"></section>
+    <section id="section-hero" class="section">
+      <div class="container">
+     			<ul class="controls">
+            <li><a class="active" href="">Home</a></li>
+            <li><a href="#section-features">Features</a></li>
+            <li><a href="#section-call-to-action">Details</a></li>
+            <li><a href="#section-contact">Contact</a></li>
+            <li><a href="#section-pricing">Pricing</a></li>
+            <li><a href="#section-footer">Footer</a></li>
+				  </ul>
+          <div class="carousel-hero">
+            <div class="carousel-item">
+              <h2>Startup 3</h2>
+              <h1>Forget About Code</h1>
+              <h5>Startup Framework gives you complete freedom over your creative process — you don’t have to think about any technical aspects. There are no limits and absolutely no coding. </h5>
+            </div>
+          </div>
+      </div>
+    </section>
   </header>
 
   <main>
-    <section class="section section-features"></section>
-    <section class="section section-call-to-action"></section>
-    <section class="section section-contact"></section>
-    <section class="section section-pricing"></section>
+    <section id="section-features" class="section"></section>
+    <section id="section-call-to-action" class="section"></section>
+    <section id="section-contact" class="section"></section>
+    <section id="section-pricing" class="section"></section>
 
   </main>
 

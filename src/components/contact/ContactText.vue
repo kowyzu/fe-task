6ba44b6dfbfe6e3da526fbd4a6ed83ebd7477500
Kowyzu <kowyzu@gmail.com>
2025-07-04 00:13:05 +0200

    feat: split contact section into ContactView.vue and ContactText.vue components

---
+++ src/components/contact/ContactText.vue
@@ -0,0 +1,27 @@
+<script setup>
+import phoneIcon from '../../images/phone-icon.svg'
+import buildingIcon from '../../images/building-icon.svg'
+import envelopeIcon from '../../images/envelope-icon.svg'
+</script>
+
+<template>
+  <h3>Let’s Keep in Touch</h3>
+  <h5>We have created a new product that will help designers, developers and companies create websites for
+    their startups quickly and easily.</h5>
+  <ul class="list-unstyled">
+    <li class="d-flex align-items-start">
+      <img :src="phoneIcon" alt="icon of phone" class="me-3" width="24" height="24" />
+      <p>+1 555 505 5050</p>
+    </li>
+    <li class="d-flex align-items-start">
+      <img :src="envelopeIcon" alt="icon of envelope" class="me-3" width="24" height="24" />
+      <p>info@designmodo.com</p>
+    </li>
+    <li class="d-flex align-items-start">
+      <img :src="buildingIcon" alt="icon of building" class="me-3" width="24" height="24" />
+      <p>
+        San Francisco, CA 560 Bush St & 20th Ave, Apt 5 San Francisco, 230909
+      </p>
+    </li>
+  </ul>
+</template>
---
+++ src/components/contact/ContactView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import ContactText from './ContactText.vue';
+</script>
+
+<template>
+  <section id="section-contact" class="section">
+    <div class="container-fluid">
+      <div class="row text-start justify-content-center">
+        <div class="col-6 contact-text p-0">
+          <ContactText />
+        </div>
+        <div class="col-6 p-0 ">
+          <div class="contact-form container">
+            <div class="row">
+              <div class="col">
+                <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+              </div>
+              <div class="col">
+                <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>

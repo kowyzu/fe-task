6cec08093f6814c9f012a5f2f1843d19b9f5e0e4
Kowyzu <kowyzu@gmail.com>
2025-07-05 15:57:10 +0200

    feat: introduce PricingTableHeading.vue; start responsive design of pricing tables

---
+++ src/components/pricing/PricingTableHeading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pricing-table-heading">
+    <h5>{{ planName }}</h5>
+    <h2>{{ planPrice }}<sup class="currency ms-2">$</sup></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    planName: String,
+    planPrice: Number
+  }
+}
+</script>
--- src/components/pricing/PricingView.vue
+++ src/components/pricing/PricingView.vue
@@ -7,65 +7,40 @@
           <h5>Startup Framework is free forever — you only pay for custom domain hosting or to export your site.</h5>
         </div>
       </div>
-      <div class="row justify-content-center gx-5">
-        <div class="col-3 pricing-table">
-          <div class="pricing-table-heading">
-            <h5>Starter</h5>
-            <h2>9.99<sup class="currency ms-2">$</sup></h2>
-          </div>
+      <div class="row justify-content-center row-cols-1 row-cols-md-3">
+        <div class="col pricing-table">
+          <PricingTableHeading :planName="starterPlanData.name" :planPrice="starterPlanData.price" />
           <ul class="p-0">
-            <li class="checked-list-item">
-              <span class="check-icon"></span>
-              <p>2 GB of space</p>
-            </li>
-            <li class="checked-list-item">
-              <span class="check-icon"></span>
-              <p>14 days of backups</p>
-            </li>
-            <li class="checked-list-item">
-              <span class="check-icon"></span>
-              <p>Social integrations</p>
-            </li>
-            <li class="checked-list-item">
-              <span class="check-icon"></span>
-              <p>Client billing</p>
-            </li>
-            <li class="list-item">
-              <p class="ps-4">Remote access</p>
-            </li>
-            <li class="list-item">
-              <p class="ps-4">Custom domain</p>
-            </li>
-            <li class="list-item">
-              <p class="ps-4">24 hours support</p>
-            </li>
-            <li class="list-item">
-              <p class="ps-4">Admin tools</p>
-            </li>
-            <li class="list-item">
-              <p class="ps-4">Collaboration tools</p>
-            </li>
-            <li class="list-item">
-              <p class="ps-4">User management</p>
-            </li>
+            <PricingTableItem v-for="(item) in starterPlanData.features" :key="item.id"
+              :class="item.isChecked ? 'checked-list-item' : 'list-item'" :text="item.text"
+              :isChecked="item.isChecked" />
           </ul>
           <div class="row justify-content-center">
             <button class="btn-action btn-big mt-4">Get Started</button>
           </div>
         </div>
-        <div class="col-3 pricing-table">
-          <div class="pricing-table-heading">
-            <h5>Starter</h5>
-            <h2>9.99<sup class="currency ms-2">$</sup></h2>
-          </div>
-          <ul>
-            <div v-for="(item, index) in starterPlanData" :key="item.id">
-              <PricingTableItem :class="item.isChecked ? 'checked-list-item' : 'list-item'" :text="item.text"
-                :isChecked="item.isChecked" />
-            </div>
+        <div class="col pricing-table mx-md-4 mx-0">
+          <PricingTableHeading :planName="professionalPlanData.name" :planPrice="professionalPlanData.price" />
+          <ul class="p-0">
+            <PricingTableItem v-for="(item) in professionalPlanData.features" :key="item.id"
+              :class="item.isChecked ? 'checked-list-item' : 'list-item'" :text="item.text"
+              :isChecked="item.isChecked" />
           </ul>
+          <div class="row justify-content-center">
+            <button class="btn-action btn-big mt-4">Get Started</button>
+          </div>
         </div>
-
+        <div class="col pricing-table">
+          <PricingTableHeading :planName="teamPlanData.name" :planPrice="teamPlanData.price" />
+          <ul class="p-0">
+            <PricingTableItem v-for="(item) in teamPlanData.features" :key="item.id"
+              :class="item.isChecked ? 'checked-list-item' : 'list-item'" :text="item.text"
+              :isChecked="item.isChecked" />
+          </ul>
+          <div class="row justify-content-center m-0 p-0">
+            <button class="btn-action btn-big mt-3">Get Started</button>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -73,18 +48,21 @@
 
 <script>
 import PricingTableItem from './PricingTableItem.vue';
+import PricingTableHeading from './PricingTableHeading.vue';
 
 //Text
 import text_JSON from './pricing.json'
 
 export default {
   components: {
-    PricingTableItem
+    PricingTableItem,
+    PricingTableHeading
   },
   data() {
     return {
-      textJson: text_JSON,
-      starterPlanData: text_JSON[0].features,
+      starterPlanData: text_JSON[0],
+      professionalPlanData: text_JSON[1],
+      teamPlanData: text_JSON[2],
     }
   },
 };
